cf77b294e8bcbfe63671db3426803563fb6813a7
Sagar Bhagwat <bhagwatsagar2290@gmail.com>
2023-01-09 20:21:45 +0530

    resolved remained pauses

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,17 +3,7 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
-
-  // DEV ONLY!!!
-  await pause(1000);
   return response.data;
 });
-
-// DEV ONLY!!!
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchUsers };
